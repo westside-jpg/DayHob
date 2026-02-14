9b59f47fee08f44b3e254f0c1990d2812c9851a8
Daniil Simakov <simakov0310@gmail.com>
2026-02-15 00:06:14 +1000

    add HTMLResponses so as not to forget

--- routers/auth.py
+++ routers/auth.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/login")
+@router.get("/login", response_class=HTMLResponse)
 def login_page(request: Request):
     return templates.TemplateResponse("auth/login.html", {"request": request})
 
-@router.get("/register")
+@router.get("/register", response_class=HTMLResponse)
 def register_page(request: Request):
     return templates.TemplateResponse("auth/register.html", {"request": request})
