dbb855ed0067ac5397b90ed17d5b8cece9b80b96
Daniil Simakov <simakov0310@gmail.com>
2026-03-01 18:57:10 +1000

    add new tables to models.py, start write feed.html

--- models.py
+++ models.py
@@ -3,6 +3,8 @@ from sqlalchemy import String, ForeignKey, Date, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from typing import Annotated
 
+from sqlalchemy.sql.schema import UniqueConstraint
+
 created_at = Annotated[datetime, mapped_column(DateTime(timezone=True), server_default=func.now())]
 
 class Base(DeclarativeBase):
@@ -18,7 +20,6 @@ class Users(Base):
 
 class PendingUsers(Base):
     __tablename__ = "pending_users"
-
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(255))
@@ -31,7 +32,6 @@ class Tasks(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     date: Mapped[date] = mapped_column(Date, unique=True, nullable=False)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
-    description: Mapped[str] = mapped_column(String(2000))
 
 class Posts(Base):
     __tablename__ = "posts"
@@ -41,3 +41,27 @@ class Posts(Base):
     text: Mapped[str] = mapped_column(String(1024))
     image_url: Mapped[str | None] = mapped_column(String(500))
     created_at: Mapped[created_at]
+
+class Likes(Base):
+    __tablename__ = "likes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable=False)
+    liked_at: Mapped[created_at]
+
+class Comments(Base):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable=False)
+    text: Mapped[str] = mapped_column(String(1024))
+    created_at: Mapped[created_at]
+
+class Followers(Base):
+    __tablename__ = "followers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    follower_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    following_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    followed_at: Mapped[created_at]
+
+    __table_args__ = (UniqueConstraint("follower_id", "following_id"),)
