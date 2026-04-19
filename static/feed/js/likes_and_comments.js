e4de698ae403fbbee370bb8090f70a4f093dff58
Daniil Simakov <simakov0310@gmail.com>
2026-04-19 15:09:37 +1000

    fix likes and comments clicks

--- static/feed/js/likes_and_comments.js
+++ static/feed/js/likes_and_comments.js
@@ -10,3 +10,16 @@ document.querySelectorAll('.like').forEach(btn => {
         }
     })
 })
+
+document.querySelectorAll('.comment').forEach(btn => {
+    btn.addEventListener('click', () => {
+        const icon = btn.querySelector('.comment-icon')
+        if (btn.classList.contains('commented')) {
+            btn.classList.remove('commented')
+            icon.src = '/static/feed/images/comment.svg'
+        } else {
+            btn.classList.add('commented')
+            icon.src = '/static/feed/images/comment-filled.svg'
+        }
+    })
+})
